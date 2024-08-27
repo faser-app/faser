f9faccdb829e92b98245ca32f3e493fa15ba7f33
neo <kremersneo0@gmail.com>
2024-08-27 14:30:57 +0200

    added ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: "Faser",
+      exec_mode: "cluster",
+      instances: "max",
+      script: "./.output/server/index.mjs",
+      port: 3100,
+    },
+  ],
+};
