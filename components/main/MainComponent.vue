dbf1179a5a64886ada3dd4e67c8506cb37cb255a
neo <kremersneo0@gmail.com>
2024-08-29 09:18:03 +0200

    added blob on index page

--- components/main/MainComponent.vue
+++ components/main/MainComponent.vue
@@ -11,5 +11,70 @@
       </div>
     </div>
   </div>
+  <div class="blob bg-gradient-to-tr z-0 pointer-events-none from-[#24c7ce] to-[#1ed794]"></div>
 </template>
-<script setup></script>
+<script setup>
+onMounted(() => {
+  document.addEventListener("mousemove", (e) => {
+    const blob = document.querySelector(".blob");
+
+    const scrollX = window.scrollX;
+    const scrollY = window.scrollY;
+
+    const targetX = e.clientX + scrollX;
+    const targetY = e.clientY + scrollY;
+
+    let blobX = parseFloat(getComputedStyle(blob).left) || 0;
+    let blobY = parseFloat(getComputedStyle(blob).top) || 0;
+
+    function animateBlob() {
+      const speed = 0.1;
+
+      blobX += (targetX - blobX - 250) * speed;
+      blobY += (targetY - blobY - 200) * speed;
+
+      blob.style.left = `${blobX}px`;
+      blob.style.top = `${blobY}px`;
+
+      requestAnimationFrame(animateBlob);
+    }
+
+    requestAnimationFrame(animateBlob);
+  });
+
+  document.addEventListener("mouseenter", (e) => {
+    const blob = document.querySelector(".blob");
+    blob.style.opacity = 1;
+  });
+
+  document.addEventListener("mouseleave", (e) => {
+    const blob = document.querySelector(".blob");
+    blob.style.opacity = 0;
+  });
+});
+</script>
+
+<style scoped>
+.blob {
+  position: absolute;
+  top: 50rem;
+  left: 30rem;
+  width: 500px;
+  opacity: 0;
+  height: 400px;
+  filter: blur(10rem);
+  transition: opacity 250ms ease;
+  animation: rotate 20s linear infinite;
+  background-color: #24c7ce;
+  border-radius: 100%;
+}
+
+@keyframes rotate {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
