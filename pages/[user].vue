0f51f54e9ea0f76381f2f28011bbcd40eaf9f07e
neo <kremersneo0@gmail.com>
2024-08-30 14:57:25 +0200

    added get Profile page

---
+++ pages/[user].vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import GetProfileComponent from '~/components/account/profile/GetProfileComponent.vue';
+</script>
+
+<template>
+    <HeaderComponent />
+    <GetProfileComponent />
+</template>
