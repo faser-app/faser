74374be871e203afdfb3ba4b59ace6efb4610ad2
neo <kremersneo0@gmail.com>
2024-09-01 17:56:43 +0200

    made search better

--- components/global/SearchUserComponent.vue
+++ components/global/SearchUserComponent.vue
@@ -12,7 +12,12 @@
     v-if="query.length > 0 && focus"
     class="md:absolute top-16 rounded-xl text-white right-32 bg-gray-700 md:w-64 p-2"
   >
-    <div v-for="user in users" :key="user.username" class="p-2">
+    <div
+      v-if="users.length !== 0"
+      v-for="user in users"
+      :key="user.username"
+      class="p-2"
+    >
       <div class="flex items-center">
         <RouterLink :to="'/' + user.username" class="flex items-center">
           <img
@@ -31,9 +36,24 @@
           >
             <i class="fa-solid fa-user rounded-full"></i>
           </div>
-          <p>{{ user.username }}</p>
+          <div class="flex">
+            <p>{{ user.username }}</p>
+            <div v-if="user.verifiedAccount" class="ml-2">
+              <div
+                class="flex justify-center text-xs items-center bg-sky-600 border w-6 h-6 border-sky-300 rounded-full"
+              >
+                <i class="fa-solid verifiedBadge fa-check"></i>
+              </div>
+            </div>
+            <div v-if="user.privateAccount" class="ml-2">
+              <i class="fa-solid fa-lock"></i>
+            </div>
+          </div>
         </RouterLink>
       </div>
+    </div>
+    <div v-else>
+      <p>No users found</p>
     </div>
   </div>
 </template>
@@ -75,3 +95,9 @@ function searchUsers() {
   }
 }
 </script>
+
+<style scoped>
+.verifiedBadge {
+  transform: translateY(1px);
+}
+</style>
