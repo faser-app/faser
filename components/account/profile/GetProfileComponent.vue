b99cf0740b4b10e5c5c58fcfbc81f622a568ebc5
neo <kremersneo0@gmail.com>
2024-09-05 13:42:44 +0200

    fixed mobile design

--- components/account/profile/GetProfileComponent.vue
+++ components/account/profile/GetProfileComponent.vue
@@ -13,7 +13,7 @@
         </p>
         <p v-if="privateAccount">Private account</p>
       </div>
-      <div class="md:w-4/5 mr-4">
+      <div class="md:w-4/5 w-full mr-4">
         <div class="flex flex-wrap bg-gray-800 md:w-full ml-2 md:ml-1 rounded-xl items-center mr-2 h-fit">
           <img v-if="hasProfilePicture && imageLoaded" :src="'https://api.faser.app/api/profile/getProfilePhoto?username=' +
             route.params.user.replace('@', '')
@@ -77,7 +77,7 @@
           <p class="w-full pl-5 pb-3">Member since {{ sinceString }}</p>
         </div>
         <div class="flex flex-wrap mt-2 bg-gray-800 md:w-full ml-2 md:ml-1 rounded-xl items-center mr-2 h-fit">
-          <div class="flex p-2 flex-wrap mt-2 bg-gray-800 w-full ml-2 md:ml-1 rounded-xl items-center mr-2 h-fit">
+          <div class="p-2 mt-2 bg-gray-800 w-full ml-2 md:ml-1 rounded-xl items-center h-fit">
             <div class="w-full flex justify-center">
               <p class="text-xl mt-2 mb-2">Posts</p>
             </div>
