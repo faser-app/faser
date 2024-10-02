33dc5f8bac848a345d957e4cc65af09ee1e10559
neo <kremersneo0@gmail.com>
2024-10-02 22:12:51 +0200

    Small changes

--- app.vue
+++ app.vue
@@ -1,11 +1,10 @@
 <script setup>
 import { definePageMeta } from '#imports';
+import Cookies from "js-cookie";
 
 definePageMeta({
   middleware: "main-meta"
-})
-
-import Cookies from "js-cookie"
+})  
 
 const accepted = ref(false)
 
