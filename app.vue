4882fca053d419d22fe139b8ca81a0136603a918
neo <kremersneo0@gmail.com>
2024-10-03 14:07:56 +0200

    Added logged in value to Google Analytics

--- app.vue
+++ app.vue
@@ -1,6 +1,19 @@
 <script setup>
 import { definePageMeta } from '#imports';
+import { useGtag } from '#imports';
 import Cookies from "js-cookie";
+
+const gTag = useGtag()
+
+if(Cookies.get("token")) {
+  gTag.gtag('event', 'logged_in', {
+    'logged_in': 'true'
+  });
+} else {
+  gTag.gtag('event', 'not_logged_in', {
+    'not_logged_in': 'true'
+  })
+}
 
 definePageMeta({
   middleware: "main-meta"
