241d9b2886950009e8ff3c9b39ff19de26b7f881
neo <kremersneo0@gmail.com>
2024-10-08 11:24:50 +0200

    disabled mobile zooming

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -47,6 +47,10 @@ export default defineNuxtConfig({
           name: "apple-mobile-web-app-status-bar-style",
           content: "black",
         },
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+        }
       ],
       script: [
         {
