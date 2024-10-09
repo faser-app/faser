1cbaa7c47fc21aad077829db8f2eeee6c53a69a9
neo <kremersneo0@gmail.com>
2024-10-09 14:09:08 +0200

    Changed Colors from advanced popup

--- components/global/AdvancedComponent.vue
+++ components/global/AdvancedComponent.vue
@@ -2,7 +2,7 @@
     <Transition name="fade">
         <div class="advanced-gradient backdrop-blur-xl p-5 overflow-scroll text-center rounded-xl m-3 md:w-auto w-full max-h-[80svh] max-w-[80svw]"
             v-if="showModal">
-            <h1 class="text-2xl text-shadow font-bold mb-2 text-[#ffffff66]">
+            <h1 class="text-2xl font-bold mb-2 text-[#ffffffbb]">
                 faser Advanced</h1>
             <p>Oh hello! You've found a faser Advanced feature.</p>
             <p>Subscribe now to faser Advanced to use it!</p>
@@ -54,9 +54,14 @@
                     </div>
                 </div>
             </div>
-            <button @click="$emit('closeModal')" class="w-full bg-gray-700 mt-2 p-2 rounded-xl">
-                Cancel
-            </button>
+            <div class="flex justify-center gap-2">
+                <button @click="$emit('closeModal')" class="w-1/3 bg-gray-700 mt-2 p-2 rounded-xl">
+                    Cancel
+                </button>
+                <button @click="$emit('closeModal')" class="w-2/3 bg-[#ffffff3f] text-white border mt-2 p-2 rounded-xl">
+                    Subscribe
+                </button>
+            </div>
         </div>
     </Transition>
 </template>
@@ -109,10 +114,6 @@ function toggleAccordion(index) {
 </script>
 
 <style scoped>
-.text-shadow {
-    filter: drop-shadow(0 0 0.6rem rgb(0, 0, 0));
-}
-
 .advanced-gradient {
     background: linear-gradient(45deg, rgba(36, 199, 206, 1) 0%, rgba(30, 209, 215, 1) 30%, rgba(255, 0, 243, 1) 70%, rgba(255, 83, 0, 1) 100%);
     background-size: 800% 800%;
