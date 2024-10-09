d70691896412272b1074e3cd708e167e8fb77bea
neo <kremersneo0@gmail.com>
2024-10-09 13:05:06 +0200

    added default embed

--- pages/index.vue
+++ pages/index.vue
@@ -3,20 +3,3 @@
     <MainComponent />
   </div>
 </template>
-<script setup>
-import { useHead } from '#app';
-
-useHead({
-  title: 'Home - faser.app',
-  meta: [
-    {
-      name: 'description',
-      content: 'This is the home page'
-    },
-    {
-      name: "theme-color",
-      content: "#030712"
-    }
-  ]
-});
-</script>
