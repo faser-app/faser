f3f18928d4e32c4fb563ac140558eb3f216699e1
neo <kremersneo0@gmail.com>
2024-10-10 08:48:55 +0200

    Updated imprint and privacy

--- pages/imprint/index.vue
+++ pages/imprint/index.vue
@@ -1,41 +1,91 @@
 <template>
     <div class="min-h-screen bg-gray-950 text-white px-5 py-6">
-        <h1 class="text-2xl font-bold mb-4">Imprint</h1>
-        <p><strong>Information in accordance with Section 5 TMG:</strong></p>
-        <p>faser.app<br>
-            Schieferweg 5<br>
-            56244 Helferskirchen<br>
-            Germany</p>
+        <div class='imprint'>
+            <h1 class="text-xl font-bold">Imprint</h1>
+            <p>Information according to § 5 TMG</p>
+            <p>
+                Neo <br>
+                Schieferweg 5<br>
+                56244 Helferskirchen, Rhineland-Palatinate, Germany<br>
+            </p>
+            <p>
+                <strong>Represented by:</strong><br>
+                Neo<br>
+            </p>
+            <p>
+                <strong>Contact:</strong> <br>
+                Phone: +49 176 55498384<br>
+                Email: <a href='mailto:support@faser.app'>support@faser.app</a>
+            </p>
+            <p>
+                <strong>Disclaimer:</strong><br><br>
+                <strong>Liability for content</strong><br><br>
+                The contents of our pages were created with great care. However, we cannot assume any liability for the
+                correctness, completeness, or up-to-dateness of the contents. As a service provider, we are responsible
+                for our own content on these pages in accordance with general laws as per § 7 Abs.1 TMG. According to §§
+                8 to 10 TMG, we are not obligated as a service provider to monitor transmitted or stored third-party
+                information or to investigate circumstances that indicate illegal activity. Obligations to remove or
+                block the use of information according to general laws remain unaffected. However, liability in this
+                regard is only possible from the time of knowledge of a specific infringement. Upon becoming aware of
+                such legal violations, we will remove this content immediately.<br><br>
+                <strong>Liability for links</strong><br><br>
+                Our offer contains links to external websites of third parties, the content of which we have no
+                influence on. Therefore, we cannot accept any responsibility for this external content. The respective
+                provider or operator of the pages is always responsible for the contents of the linked pages. The linked
+                pages were checked for possible legal violations at the time of linking. Illegal contents were not
+                recognizable at the time of linking. A permanent control of the content of the linked pages is not
+                reasonable without concrete indications of an infringement. If we become aware of any infringements, we
+                will remove such links immediately.<br><br>
+                <strong>Copyright</strong><br><br>
+                The content and works created by the site operators on these pages are subject to German copyright law.
+                Duplication, processing, distribution, or any form of commercialization of such material beyond the
+                scope of the copyright law shall require the prior written consent of its respective author or creator.
+                Downloads and copies of this page are only permitted for private, non-commercial use. Insofar as the
+                content on this site was not created by the operator, third-party copyrights are respected. In
+                particular, third-party content is marked as such. Should you nevertheless become aware of a copyright
+                infringement, please inform us accordingly. Upon notification of violations, we will remove such content
+                immediately.<br><br>
+                <strong>Privacy</strong><br><br>
+                The use of our website is generally possible without providing personal data. Where personal data (such
+                as name, address, or email addresses) is collected on our pages, this is always done on a voluntary
+                basis, as far as possible. These data will not be disclosed to third parties without your explicit
+                consent.<br>
+                We would like to point out that data transmission over the Internet (e.g., when communicating by email)
+                can have security gaps. A complete protection of data against access by third parties is not
+                possible.<br>
+                The use of contact data published as part of the imprint obligation by third parties for the purpose of
+                sending unsolicited advertisements and information materials is hereby expressly rejected. The operators
+                of the pages expressly reserve the right to take legal action in the event of unsolicited promotional
+                information, such as spam emails.<br>
+                <br><br>
+                <strong>Google Analytics</strong><br><br>
+                This website uses Google Analytics, a web analytics service provided by Google Inc. ("Google"). Google
+                Analytics uses "cookies," which are text files placed on your computer, to help the website analyze how
+                users use the site. The information generated by the cookie about your use of the website (including
+                your IP address) will be transmitted to and stored by Google on servers in the United States. Google
+                will use this information for the purpose of evaluating your use of the website, compiling reports on
+                website activity for website operators, and providing other services relating to website activity and
+                internet usage. Google may also transfer this information to third parties where required to do so by
+                law, or where such third parties process the information on Google's behalf. Google will not associate
+                your IP address with any other data held by Google. You may refuse the use of cookies by selecting the
+                appropriate settings on your browser; however, please note that if you do this, you may not be able to
+                use the full functionality of this website. By using this website, you consent to the processing of data
+                about you by Google in the manner and for the purposes set out above.
+            </p>
+            <p>Imprint generated with the help of <a href="https://www.impressum-generator.de">Imprint Generator</a> by
+                <a href="https://www.kanzlei-hasselbach.de/standorte/bonn/" rel="nofollow">Kanzlei Hasselbach, Bonn</a>
+            </p>
+        </div>
 
-        <br>
-
-        <p><strong>Contact:</strong></p>
-        <p>Phone: +49 176 55498384<br>
-            Email: <a href="mailto:support@faser.app" class="underline">support@faser.app</a></p>
-
-        <br>
-
-        <p><strong>Data Protection Officer:</strong></p>
-        <p>For privacy-related inquiries, please contact:<br>
-            Email: <a href="mailto:support@faser.app" class="underline">support@faser.app</a></p>
-        <p>For more information on how we process personal data, please review our <a href="/privacy"
-                class="underline">Privacy Policy</a>.</p>
-
-        <br>
-
-        <p><strong>Disclaimer:</strong></p>
-        <p>The provider assumes no liability for the accuracy, completeness, and timeliness of the information provided.
-        </p>
-
-        <br>
-
-        <p><strong>Liability for Links:</strong></p>
-        <p>Our offer contains links to external websites of third parties. We have no influence on the content of these
-            linked pages.</p>
-
-        <br>
-
-        <p><strong>Copyright:</strong></p>
-        <p>The content and works created by the site operators on these pages are subject to German copyright law.</p>
     </div>
 </template>
+
+<style scoped>
+a {
+    text-decoration: underline;
+}
+
+p {
+    margin-bottom: 1rem;
+}
+</style>
