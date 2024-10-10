a2a6710f9cdba5649ce51cd329b1a2c947022bc2
neo <kremersneo0@gmail.com>
2024-10-10 09:23:48 +0200

    Updated Terms of Service

--- pages/tos/index.vue
+++ pages/tos/index.vue
@@ -6,56 +6,205 @@
             and apply to all users aged 13 and older. By accessing or using the App, you agree to be bound by these
             Terms of Service. If you do not agree, please do not use the App.</p><br>
 
-        <h2 class="text-xl font-semibold mt-6 mb-2">1. Eligibility</h2>
-        <p>The App is available only to users who are 13 years of age or older.</p>
-        <p>Users between the ages of 13 and 18 must have parental or legal guardian consent to use the App.</p>
-        <p>By using the App, you confirm that you are at least 13 years old or have the consent of a parent or legal
-            guardian.</p><br>
+        <h2 class="text-xl font-semibold mt-6 mb-2">1. User Eligibility and Account</h2>
+        <div class="ml-5">
+            <h3 class="text-lg font-semibold mt-4">1.1 Age Requirement</h3>
+            <p>The App is available only to users who are 13 years of age or older. Users below this age are not
+                permitted
+                to create an account or use the App's services.</p>
+            <h3 class="text-lg font-semibold mt-4">1.2 Parental Consent</h3>
+            <p>Users between the ages of 13 and 18 must have parental or legal guardian consent to use the App. It is
+                the
+                responsibility of the parents or guardians to monitor their child’s use of the App.</p>
 
-        <h2 class="text-xl font-semibold mt-6 mb-2">2. User Account</h2>
-        <p>To access certain features of the App, you may need to create a user account. You agree to provide accurate,
-            current, and complete information.</p>
-        <p>You are responsible for maintaining the confidentiality of your password and for any activities that occur
-            under your account.</p><br>
+            <h3 class="text-lg font-semibold mt-4">1.3 Age Confirmation</h3>
+            <p>By using the App, you confirm that you are at least 13 years old or have the consent of a parent or legal
+                guardian. You may be required to provide proof of your age or consent if requested.</p>
 
-        <h2 class="text-xl font-semibold mt-6 mb-2">3. Code of Conduct</h2>
-        <p>You agree not to use the App to post any unlawful, offensive, abusive, defamatory, obscene, or otherwise
-            inappropriate content.</p>
-        <p>You may not upload content that is copyrighted or that infringes on the intellectual property rights of
-            others unless you have the necessary rights or permissions.</p>
-        <p>Bullying, stalking, threats, hate speech, or discrimination based on race, religion, gender, sexual
-            orientation, or any other protected characteristic is strictly prohibited.</p><br>
+            <h3 class="text-lg font-semibold mt-4">1.4 Account Creation</h3>
+            <p>To access certain features of the App, you may need to create a user account. You agree to provide
+                accurate,
+                current, and complete information during the registration process.</p>
 
-        <h2 class="text-xl font-semibold mt-6 mb-2">4. Privacy</h2>
-        <p>Our Privacy Policy outlines how we collect, use, and protect your personal data. By using the App, you agree
-            to the collection and use of your data in accordance with our Privacy Policy.</p><br>
+            <h3 class="text-lg font-semibold mt-4">1.5 Account Security</h3>
+            <p>You are responsible for maintaining the confidentiality of your password and for any activities that
+                occur
+                under your account. You should notify us immediately of any unauthorized use of your account.</p>
 
-        <h2 class="text-xl font-semibold mt-6 mb-2">5. Content and Intellectual Property</h2>
-        <p>All content you upload or share on the App remains your property. However, by uploading content, you grant us
-            a non-exclusive, worldwide, royalty-free right to use that content in connection with the operation of the
-            App.</p>
-        <p>We reserve the right to remove any content that violates these Terms of Service.</p><br>
+            <h3 class="text-lg font-semibold mt-4">1.6 User Responsibilities</h3>
+            <p>Users are responsible for maintaining the accuracy of their information and ensuring their account
+                credentials remain secure. It is your responsibility to update your information if there are any
+                changes.
+            </p>
 
-        <h2 class="text-xl font-semibold mt-6 mb-2">6. Limitation of Liability</h2>
-        <p>The App is provided "as is" without any warranties. We do not guarantee the accuracy, reliability, or
-            availability of the App.</p>
-        <p>We are not liable for any direct, indirect, incidental, special, or consequential damages arising from your
-            use of the App.</p><br>
+            <h3 class="text-lg font-semibold mt-4">1.7 Account Suspension or Termination</h3>
+            <p>We reserve the right to suspend or terminate your account at any time, without notice, if you violate
+                these
+                Terms of Service or engage in any behavior that we consider harmful to other users or the App.</p>
 
-        <h2 class="text-xl font-semibold mt-6 mb-2">7. Changes to Terms of Service</h2>
-        <p>We reserve the right to modify these Terms of Service at any time. Changes will be effective upon posting the
-            revised terms in the App. Continued use of the App after such changes constitutes acceptance of the new
-            terms.</p><br>
+            <h3 class="text-lg font-semibold mt-4">1.8 Voluntary Account Termination</h3>
+            <p>You may terminate your account at any time by contacting us. Upon termination, you will no longer have
+                access
+                to your account or any content stored on the App.</p><br>
+        </div>
 
-        <h2 class="text-xl font-semibold mt-6 mb-2">8. Termination</h2>
-        <p>We reserve the right to suspend or terminate your account at any time, without notice, if you violate these
-            Terms of Service.</p><br>
 
-        <h2 class="text-xl font-semibold mt-6 mb-2">9. Governing Law</h2>
-        <p>These Terms of Service are governed by the laws of Germany, where the provider of the App is based.</p><br>
+        <h2 class="text-xl font-semibold mt-6 mb-2">2. Code of Conduct</h2>
+        <div class="ml-5">
+            <h3 class="text-lg font-semibold mt-4">2.1 Appropriate Use</h3>
+            <p>You agree not to use the App to post any unlawful, offensive, abusive, defamatory, obscene, or otherwise
+                inappropriate content. Content that violates the rights of others or applicable laws is strictly
+                prohibited.
+            </p>
 
-        <h2 class="text-xl font-semibold mt-6 mb-2">10. Contact</h2>
-        <p>For any questions or comments regarding these Terms of Service, please contact us at 
-            <a href="mailto:support@faser.app" class="underline">support@faser.app</a>.</p>
+            <h3 class="text-lg font-semibold mt-4">2.2 Intellectual Property Rights</h3>
+            <p>You may not upload content that is copyrighted or that infringes on the intellectual property rights of
+                others unless you have the necessary rights or permissions. Violating this rule may result in account
+                suspension.</p>
+
+            <h3 class="text-lg font-semibold mt-4">2.3 Prohibited Behavior</h3>
+            <p>Bullying, stalking, threats, hate speech, or discrimination based on race, religion, gender, sexual
+                orientation, or any other protected characteristic is strictly prohibited and will result in immediate
+                action against the offending account.</p><br>
+        </div>
+
+        <h2 class="text-xl font-semibold mt-6 mb-2">3. Content and Intellectual Property</h2>
+        <div class="ml-5">
+            <h3 class="text-lg font-semibold mt-4">3.1 Ownership of Content</h3>
+            <p>All content you upload or share on the App remains your property. However, by uploading content, you
+                grant us
+                a non-exclusive, worldwide, royalty-free right to use that content in connection with the operation of
+                the
+                App.</p>
+
+            <h3 class="text-lg font-semibold mt-4">3.2 Content Removal</h3>
+            <p>We reserve the right to remove any content that violates these Terms of Service, including content that
+                we
+                determine to be offensive, harmful, or inappropriate for the platform.</p>
+
+            <h3 class="text-lg font-semibold mt-4">3.3 License to Use Content</h3>
+            <p>By posting content on the App, you grant us a license to use, modify, and display your content for the
+                purposes of operating, promoting, and improving the App. This license ends when you remove your content
+                or
+                delete your account.</p><br>
+        </div>
+
+        <h2 class="text-xl font-semibold mt-6 mb-2">4. Payment and Subscriptions</h2>
+        <div class="ml-5">
+            <h3 class="text-lg font-semibold mt-4">4.1 Subscription Fees</h3>
+            <p>If you choose to purchase a subscription, you agree to pay the fees specified for the selected
+                subscription
+                plan. Fees are billed in advance and are non-refundable.</p>
+
+            <h3 class="text-lg font-semibold mt-4">4.2 Automatic Renewal</h3>
+            <p>Subscriptions will automatically renew unless canceled prior to the renewal date. You can manage your
+                subscription and cancel it through your account settings.</p>
+
+            <h3 class="text-lg font-semibold mt-4">4.3 Refund Policy</h3>
+            <p>All payments are non-refundable unless otherwise required by law. If you experience issues with your
+                subscription, please contact us for assistance.</p><br>
+        </div>
+
+        <h2 class="text-xl font-semibold mt-6 mb-2">5. Privacy and Data Security</h2>
+        <div class="ml-5">
+            <h3 class="text-lg font-semibold mt-4">5.1 Data Collection</h3>
+            <p>Our Privacy Policy outlines how we collect, use, and protect your personal data. By using the App, you
+                agree
+                to the collection and use of your data in accordance with our Privacy Policy.</p>
+
+            <h3 class="text-lg font-semibold mt-4">5.2 Security Measures</h3>
+            <p>We implement reasonable security measures to protect your data. However, no internet-based service is
+                completely secure, and we cannot guarantee absolute security. It is your responsibility to protect your
+                account credentials.</p><br>
+        </div>
+
+        <h2 class="text-xl font-semibold mt-6 mb-2">6. Third-Party Services</h2>
+        <div class="ml-5">
+            <h3 class="text-lg font-semibold mt-4">6.1 Links to External Services</h3>
+            <p>The App may contain links to third-party websites or services that are not owned or controlled by us.
+                These
+                links are provided for convenience only, and we are not responsible for the content or practices of
+                these
+                external services.</p>
+
+            <h3 class="text-lg font-semibold mt-4">6.2 No Liability for Third-Party Services</h3>
+            <p>We are not responsible for the content, privacy policies, or practices of any third-party websites or
+                services. Use of such services is at your own risk and subject to their respective terms.</p>
+
+            <h3 class="text-lg font-semibold mt-4">6.3 Third-Party Terms</h3>
+            <p>Use of third-party services is subject to their own terms and conditions. We encourage you to review
+                those
+                terms before using any third-party service integrated with our App.</p><br>
+        </div>
+
+        <h2 class="text-xl font-semibold mt-6 mb-2">7. Limitation of Liability</h2>
+        <div class="ml-5">
+            <h3 class="text-lg font-semibold mt-4">7.1 No Warranties</h3>
+            <p>The App is provided "as is" without any warranties. We do not guarantee the accuracy, reliability, or
+                availability of the App or its services.</p>
+
+            <h3 class="text-lg font-semibold mt-4">7.2 Limitation of Damages</h3>
+            <p>We are not liable for any direct, indirect, incidental, special, or consequential damages arising from
+                your
+                use of the App, including any data loss or loss of access to the App.</p><br>
+        </div>
+
+        <h2 class="text-xl font-semibold mt-6 mb-2">8. Changes to Terms of Service</h2>
+        <div class="ml-5">
+            <h3 class="text-lg font-semibold mt-4">8.1 Right to Modify</h3>
+            <p>We reserve the right to modify these Terms of Service at any time. You will be notified of significant
+                changes through the App or via email.</p>
+
+            <h3 class="text-lg font-semibold mt-4">8.2 Effective Date of Changes</h3>
+            <p>Changes will be effective upon posting the revised terms in the App. It is your responsibility to review
+                the
+                Terms regularly for updates.</p>
+
+            <h3 class="text-lg font-semibold mt-4">8.3 Acceptance of Changes</h3>
+            <p>Continued use of the App after changes to the Terms constitutes acceptance of the new terms. If you do
+                not
+                agree to the changes, you must stop using the App.</p><br>
+        </div>
+
+        <h2 class="text-xl font-semibold mt-6 mb-2">9. Dispute Resolution</h2>
+        <div class="ml-5">
+            <h3 class="text-lg font-semibold mt-4">9.1 Informal Resolution</h3>
+            <p>In the event of any dispute arising out of or in connection with these Terms of Service, you agree to
+                first
+                attempt to resolve the dispute informally by contacting us at support@faser.app.</p>
+
+            <h3 class="text-lg font-semibold mt-4">9.2 Mediation</h3>
+            <p>If a dispute cannot be resolved informally, it will be resolved through mediation before taking legal
+                action.
+                Mediation will take place in Germany.</p><br>
+        </div>
+
+        <h2 class="text-xl font-semibold mt-6 mb-2">10. Governing Law</h2>
+        <div class="ml-5">
+            <h3 class="text-lg font-semibold mt-4">10.1 Jurisdiction</h3>
+            <p>These Terms of Service are governed by the laws of Germany, where the provider of the App is based. Any
+                legal
+                proceedings arising from or related to these Terms will be brought in the courts of Germany.</p><br>
+        </div>
+
+        <h2 class="text-xl font-semibold mt-6 mb-2">11. Feedback and Suggestions</h2>
+        <div class="ml-5">
+            <h3 class="text-lg font-semibold mt-4">11.1 Voluntary Feedback</h3>
+            <p>Any feedback, comments, or suggestions you may provide regarding the App are entirely voluntary. We
+                appreciate your input, which helps us improve our services.</p>
+
+            <h3 class="text-lg font-semibold mt-4">11.2 Rights to Feedback</h3>
+            <p>We are free to use any feedback without any obligation to you, including without limitation, improving
+                the
+                App and its features.</p><br>
+        </div>
+
+        <h2 class="text-xl font-semibold mt-6 mb-2">12. Contact</h2>
+        <div class="ml-5">
+            <h3 class="text-lg font-semibold mt-4">12.1 Contact Information</h3>
+            <p>If you have any questions or comments regarding these Terms of Service, please contact us at
+                <a href="mailto:support@faser.app" class="underline">support@faser.app</a>.
+            </p>
+        </div>
     </div>
 </template>
