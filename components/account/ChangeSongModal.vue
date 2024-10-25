56ccf65cafee0fa910f93c1eeeed6069d10bab89
neo <kremersneo0@gmail.com>
2024-10-25 08:49:43 +0200

    Made search mobile friendly

--- components/account/ChangeSongModal.vue
+++ components/account/ChangeSongModal.vue
@@ -2,7 +2,7 @@
     <transition name="fade" @leave="leave">
         <div v-if="props.showModal" :class="{ animation: props.showModal, fadeOut: !props.showModal }"
             class="fixed flex justify-center items-center top-0 left-0 w-full h-full z-50 backdrop-blur">
-            <div class="bg-gray-900 p-5 rounded-xl m-3 md:w-auto w-full" :class="{
+            <div class="bg-gray-900 max-h-[80svh] overflow-scroll p-5 rounded-xl m-3 md:w-auto w-full" :class="{
                 'border border-red-500': error,
             }">
                 <div class="w-full flex justify-center">
