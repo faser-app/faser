2cd2b5475aeccad772acebcfa9404160b07123d6
neo <kremersneo0@gmail.com>
2024-11-21 11:05:27 +0100

    Fixed nsfw text

--- components/post/CreatePostComponent.vue
+++ components/post/CreatePostComponent.vue
@@ -72,8 +72,8 @@
                             <div
                                 class="relative w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-500">
                             </div>
-                            <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">NSFW <i
-                                    class="fa-solid fa-triangle-exclamation"></i></span>
+                            <p class="ms-3 text-sm font-medium dark:text-gray-300">NSFW <i
+                                    class="fa-solid fa-triangle-exclamation"></i></p>
                         </label>
                     </div>
                     <p v-if="error" class="text-red-500 mt-1">{{ error }}</p>
