999f6dbf25d41a8497bf62a9d7421188cb5cf82f
neo <kremersneo0@gmail.com>
2024-11-21 14:12:49 +0100

    Added admin panel to terminate accounts quickly

---
+++ pages/admin/accounts/index.vue
@@ -0,0 +1,92 @@
+<template>
+    <div class="min-h-screen text-white px-2">
+        <div class="flex w-full gap-2" v-if="loaded">
+            <div class="w-1/2">
+                <h1>Active Accounts</h1>
+
+                <div v-for="account in activeAccounts" :key="account.id">
+                    <div class="flex items-center gap-2 mb-4">
+                        <div class="flex justify-between w-full">
+                            <div class="flex items-center gap-2">
+                                <img :src="'https://api.faser.app/api/profile/getProfilePhoto?username=' + account.username"
+                                    class="h-12 w-12 rounded-full" />
+                                <div>
+                                    <p>{{ account.username }}</p>
+                                </div>
+                            </div>
+                            <div>
+                                <button class="bg-red-500 px-2 py-1 rounded" @click="terminateAccount(account.id)">
+                                    Terminate
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="border-r border-gray-700 min-h-screen"></div>
+            <div class="w-1/2">
+                <h1>Terminated Accounts</h1>
+
+                <div v-for="account in terminatedAccounts" :key="account.id">
+                    <div class="flex items-center gap-2">
+                        <img :src="'https://api.faser.app/api/profile/getProfilePhoto?username=' + account.username"
+                            class="h-12 w-12 rounded-full" />
+                        <div>
+                            <p>{{ account.displayName }}</p>
+                            <p>{{ account.username }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import axios from 'axios'
+import Cookies from 'js-cookie'
+
+const token = Cookies.get('token')
+const terminatedAccounts = ref([])
+const activeAccounts = ref([])
+const loaded = ref(false)
+
+onMounted(() => {
+    loadList()
+})
+
+function loadList() {
+    axios.post("https://api.faser.app/api/admin/getAccounts", {
+        token: token
+    })
+        .then((response) => {
+            terminatedAccounts.value = response.data.terminatedProfiles
+            activeAccounts.value = response.data.activeProfiles
+        })
+}
+
+function terminateAccount(id) {
+    axios.post("https://api.faser.app/api/admin/terminateAccount", {
+        token: token,
+        accountId: id
+    })
+        .then((response) => {
+            loadList()
+        })
+}
+
+axios.get("https://api.faser.app/api/account/getOwnProfile", {
+    headers: {
+        token: token
+    }
+})
+    .then((response) => {
+        if (!response.data[1].admin) {
+            router.push("/?error=You%20are%20not%20an%20admin")
+        } else {
+            loaded.value = true
+        }
+
+    })
+
+</script>
--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,5 +1,33 @@
 <template>
-    <div>
-        <AdminDisabledPosts />
+    <div class="min-h-screen">
+        <div class="flex w-full justify-center gap-4 mt-2" v-if="loaded">
+            <RouterLink to="/admin/posts" class="py-3 px-5 rounded-xl bg-gray-800 text-white">Posts</RouterLink>
+            <RouterLink to="/admin/accounts" class="py-3 px-5 rounded-xl bg-gray-800 text-white">Accounts</RouterLink>
+        </div>
     </div>
 </template>
+
+<script setup>
+import axios from 'axios'
+import Cookies from 'js-cookie'
+import { useRouter } from 'vue-router';
+
+const token = Cookies.get('token')
+const router = useRouter()
+const loaded = ref(false)
+
+axios.get("https://api.faser.app/api/account/getOwnProfile", {
+    headers: {
+        token: token
+    }
+})
+.then((response) => {
+    if(!response.data[1].admin) {
+        router.push("/?error=You%20are%20not%20an%20admin")
+    } else {
+        loaded.value = true
+    }
+
+})
+
+</script>
