d1fd554842dd2dbe119e06ee3c37e2469ce2b27c
onemo0 <kremersneo0@gmail.com>
2025-03-25 08:01:42 +0100

    Fixed community link design

--- components/account/profile/CommunityLinkComponent.vue
+++ components/account/profile/CommunityLinkComponent.vue
@@ -2,13 +2,16 @@
     <div class="w-full mb-2 flex mr-8 h-12 truncate items-center pr-3 justify-between rounded-md"
         :style="{ backgroundColor: currentPalette.buttonPrimary, color: currentPalette.textSecondary }">
         <div class="flex items-center">
-            <img class="h-8 w-8 m-2 rounded-full" v-if="hasImage" :src="'https://s3.faser.app/communityimages/' + community.id + '/image.png'"
+            <img class="h-8 w-8 m-2 rounded-full" v-if="hasImage"
+                :src="'https://s3.faser.app/communityimages/' + community.id + '/image.png'"
                 @error="hasImage = false" />
             <i v-else class="fa-solid fa-users p-2 mr-2 text-lg"></i>
             {{ community.displayName }}
         </div>
-        <i v-if="community.private" class="fa-solid fa-lock"></i>
-        <i v-if="profileCommunitySettings?.hidden" class="fa-solid fa-eye-slash"></i>
+        <div class="flex gap-2">
+            <i v-if="community.private" class="fa-solid fa-lock"></i>
+            <i v-if="profileCommunitySettings?.hidden" class="fa-solid fa-eye-slash"></i>
+        </div>
     </div>
 </template>
 
